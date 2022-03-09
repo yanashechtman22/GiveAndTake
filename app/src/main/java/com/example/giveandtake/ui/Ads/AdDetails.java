44b5a28278b5911ba5548a5ff9f4015065af5d88
yanashechtman22 <yanashech@gmail.com>
2022-03-09 18:21:44 +0200

    added mock to firebase, now there are docs in storage

--- app/src/main/java/com/example/giveandtake/ui/Ads/AdDetails.java
+++ app/src/main/java/com/example/giveandtake/ui/Ads/AdDetails.java
@@ -1,4 +1,4 @@
-package com.example.giveandtake.ui.addAd;
+package com.example.giveandtake.ui;
 
 import android.os.Bundle;
 
@@ -12,10 +12,10 @@ import com.example.giveandtake.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_add_ad#newInstance} factory method to
+ * Use the {@link AdDetails#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_add_ad extends Fragment {
+public class AdDetails extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class fragment_add_ad extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public fragment_add_ad() {
+    public AdDetails() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class fragment_add_ad extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_add_ad.
+     * @return A new instance of fragment AdDetails.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_add_ad newInstance(String param1, String param2) {
-        fragment_add_ad fragment = new fragment_add_ad();
+    public static AdDetails newInstance(String param1, String param2) {
+        AdDetails fragment = new AdDetails();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class fragment_add_ad extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_ad, container, false);
+        return inflater.inflate(R.layout.fragment_ad_details, container, false);
     }
 }
---
+++ app/src/main/java/com/example/giveandtake/ui/Ads/AddAdFragment.java
@@ -0,0 +1,141 @@
+package com.example.giveandtake.ui.addAd;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.giveandtake.MyApplication;
+import com.example.giveandtake.R;
+import com.example.giveandtake.model.Ad;
+import com.example.giveandtake.model.AppModel;
+import com.example.giveandtake.model.AuthenticationModel;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.UserInfo;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class AddAdFragment extends Fragment {
+    private static final int REQUEST_CAMERA = 1;
+    private static final int REQUEST_GALLERY = 100;
+    EditText nameEt;
+    EditText idEt;
+    CheckBox cb;
+    Button saveBtn;
+    Button cancelBtn;
+    Bitmap imageBitmap;
+    ImageView avatarImv;
+    ImageButton camBtn;
+    ImageButton galleryBtn;
+    View view;
+    UserInfo userInfo;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_add_ad,container, false);
+        nameEt = view.findViewById(R.id.title_et);
+        idEt = view.findViewById(R.id.content_et);
+        saveBtn = view.findViewById(R.id.main_save_btn);
+        cancelBtn = view.findViewById(R.id.main_cancel_btn);
+        avatarImv = view.findViewById(R.id.baseImage_iv);
+        userInfo = AuthenticationModel.instance.getUserInfo();
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+            }
+        });
+
+        camBtn = view.findViewById(R.id.main_cam_btn);
+        galleryBtn = view.findViewById(R.id.main_gallery_btn);
+
+        camBtn.setOnClickListener(v -> {
+            openCam();
+        });
+
+        galleryBtn.setOnClickListener(v -> {
+            openGallery();
+        });
+        return view;
+    }
+
+    private void openGallery() {
+        Intent i = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, REQUEST_GALLERY);
+    }
+
+    private void openCam() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,REQUEST_CAMERA);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CAMERA && resultCode == Activity.RESULT_OK){
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            avatarImv.setImageBitmap(imageBitmap);
+        } else if(requestCode == REQUEST_GALLERY && resultCode == Activity.RESULT_OK) {
+            Uri imageUri = data.getData();
+            try {
+                imageBitmap = MediaStore.Images.Media.getBitmap(MyApplication.getContext()
+                        .getContentResolver(), imageUri);
+                avatarImv.setImageBitmap(imageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    private void save() {
+        saveBtn.setEnabled(false);
+        cancelBtn.setEnabled(false);
+        camBtn.setEnabled(false);
+        galleryBtn.setEnabled(false);
+
+        String title = nameEt.getText().toString();
+        String content = idEt.getText().toString();
+        Long lud = new Long(0);
+        String userId = userInfo.getUid();
+
+        //TODO: this is mock! this line shouldn't be here
+        Ad ad = new Ad(content,0, "beer sheva");
+
+        if (imageBitmap == null){
+            AppModel.instance.addAd(ad,()->{
+                Snackbar.make(view, "without image, "+ ad.getContent(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            });
+        } else {
+            String adImageId = UUID.randomUUID().toString();
+            AppModel.instance.saveImage(imageBitmap, adImageId + ".jpg", url -> {
+                ad.setImageUrl(url);
+                AppModel.instance.addAd(ad,()->{
+                    Snackbar.make(view, "with image, " + ad.getContent(), Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                });
+            });
+        }
+    }
+}
