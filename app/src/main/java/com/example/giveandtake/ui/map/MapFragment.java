86fa2e9b9afe951b700b2a06c85cb608415c793b
yanashechtman22 <yanashech@gmail.com>
2022-03-18 11:46:30 +0200

    map is working with mock locations

---
+++ app/src/main/java/com/example/giveandtake/ui/map/MapFragment.java
@@ -0,0 +1,120 @@
+package com.example.giveandtake.ui.map;
+
+import static com.google.android.gms.common.GooglePlayServicesUtil.isGooglePlayServicesAvailable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.giveandtake.R;
+import com.example.giveandtake.model.Post;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapFragment extends Fragment {
+
+    MapViewModel mapViewModel;
+    private View view;
+
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            List<Post> posts = mapViewModel.getPosts();
+            for (Post post: posts){
+                String locationStr = post.getLocation();
+                LatLng location = getLocationFromAddress(locationStr);
+                if(location == null) continue;
+                Marker marker = googleMap.addMarker(new MarkerOptions().position(location).title("Marker in Haifa"));
+                marker.setTag(post.getId());
+                googleMap.setOnMarkerClickListener(marker1 -> {
+                    String postId = marker1.getTag().toString();
+                    Navigation.findNavController(view).navigate(MapFragmentDirections.actionNavMapToPostDetailsFragment(postId));
+                    return true;
+                });
+            }
+            LatLng israel = getLocationFromAddress("Israel");
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(israel,8));
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_map, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mapViewModel = new ViewModelProvider(this).get(MapViewModel.class);
+    }
+
+    public LatLng getLocationFromAddress(String strAddress) {
+
+        Geocoder coder = new Geocoder(getContext());
+        List<Address> address;
+        LatLng p1;
+
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null || address.size() == 0) {
+                return null;
+            }
+            Address location = address.get(0);
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+
+            p1 = new LatLng(latitude, longitude);
+
+            return p1;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
